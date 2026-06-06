6b3a7b0096f7af9f1d1428fef1b89db6a56412c0
Tarrah4Tammwe <133949570+Tarrah4Tammwe@users.noreply.github.com>
2026-06-06 10:59:24 +0100

    fix: restore clean metro config

--- metro.config.js
+++ metro.config.js
@@ -1,21 +1,5 @@
 const { getDefaultConfig } = require('expo/metro-config');
-const path = require('path');
 
 const config = getDefaultConfig(__dirname);
-
-// Block react-native-screens fabric TypeScript source files
-// Metro should use the compiled JS versions instead
-const originalResolveRequest = config.resolver.resolveRequest;
-config.resolver.resolveRequest = (context, moduleName, platform) => {
-  if (originalResolveRequest) {
-    return originalResolveRequest(context, moduleName, platform);
-  }
-  return context.resolveRequest(context, moduleName, platform);
-};
-
-// Exclude fabric TS files from react-native-screens
-config.resolver.blockList = [
-  /node_modules\/react-native-screens\/src\/fabric\/.*/,
-];
 
 module.exports = config;
